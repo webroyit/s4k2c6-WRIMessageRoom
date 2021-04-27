da43c166ba09fb13883ae14c6ac740710932c194
royweb <xytank@gmail.com>
2021-04-27 13:20:56 -0400

    Set up create react app

---
+++ README.md
@@ -0,0 +1,1 @@
+# WRI Message Room
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WRI Message Room</h1>
+    </div>
+  );
+}
+
+export default App;
