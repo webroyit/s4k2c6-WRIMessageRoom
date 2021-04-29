62f81026f82c38d64ffd565886511e50428ebf8f
royweb <xytank@gmail.com>
2021-04-29 13:12:54 -0400

    Install firebase and set it up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WRI Message Room
+
+## Notes
+- RealTime Database is a NoSQL database.
+- Firestore is a hybrid database (SQL and NoSQL) that can be search and has index
+- Firestore is faster than RealTime Database.
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCa8dJCGVkztjwXq20-e__PJI-Rpn3tQFg",
+    authDomain: "wri-messageroom.firebaseapp.com",
+    projectId: "wri-messageroom",
+    storageBucket: "wri-messageroom.appspot.com",
+    messagingSenderId: "482612442264",
+    appId: "1:482612442264:web:b3f54c454589a5e049cced"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// import { auth, provider } from './firebase';
+export { auth, provider };
+
+// import db from './firebase'; 
+export default db;
