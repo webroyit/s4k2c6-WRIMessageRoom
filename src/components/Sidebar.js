d835ce6e97bbcf7480df21f9d5805656257c1538
royweb <xytank@gmail.com>
2021-04-29 12:01:28 -0400

    Add sidebar option component with styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import AddIcon from '@material-ui/icons/Add';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import GroupIcon from '@material-ui/icons/Group';
+import AppsIcon from '@material-ui/icons/Apps';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
@@ -17,6 +27,18 @@ function Sidebar() {
                 </div>
                 <AddIcon />
             </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved items" />
+            <SidebarOption Icon={BookmarksIcon} title="Channel browser" />
+            <SidebarOption Icon={GroupIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={DescriptionIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" />
         </div>
     )
 }
